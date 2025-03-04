b31d1749b0e636e3a6b3d4037cde724224e66c3e
OLIVER427 <oliver427daminecraftplayer@gmail.com>
2025-03-03 18:13:44 -0600

    replaced if with switch case

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 let prevCommand
+
+document.getElementById("userAgent").innerHTML = navigator.userAgent
+
 document.addEventListener('keydown', (event) => {
     if (event.key == "Enter") {
         prevCommand = document.getElementById("input").innerHTML
         document.getElementById("outputCon").innerHTML += `<div id='outputIn'><p id="dir">usr@website <span>$</span> </p>
         <div id="output" contenteditable="false" spellcheck="false">`+ prevCommand +`</div></div>`
 
-        if (prevCommand == "help") {
+        switch(prevCommand){
+            case "help": //(emailSubject == "Subject1")
             document.getElementById("outputCon").innerHTML += `
-<pre>
+            <pre>
 To access my other projects, use " [PLACEHOLDER] "
-</pre>`
-        }else if (prevCommand == "" || prevCommand === " ") {
+            </pre>`
+               break;
 
-        } else {
-            document.getElementById("outputCon").innerHTML += `
-<pre>
+            case "": //nothing here so theres no "command not found" for an empty message
+               break;
+
+            default:
+                document.getElementById("outputCon").innerHTML += `
+                <pre>
 `+window.location.pathname+`: `+ prevCommand +`: command not found
-</pre>`
+                </pre>`
+            break;
         }
+
 
         document.getElementById("input").innerHTML = ""
         setTimeout(() => {
